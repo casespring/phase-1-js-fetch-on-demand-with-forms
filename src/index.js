8d152f23a9bd3fc1d15e14c864163b84a0544978
Marco Upia <markremembers@gmail.com>
2023-11-06 06:46:52 -0500

    code along fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
-}
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //console.log(e.target.children[1].value);
+    let input = document.querySelector("input#searchByID");
+    console.log(input.value);
+
+    
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+            const title = document.querySelector("section#movieDetails h4");
+            const sumary = document.querySelector("section#movieDetails p");
+            
+            title.innerText = data.title;
+            sumary.innerText = data.summary;
+      })
+  })
+};
 
 document.addEventListener('DOMContentLoaded', init);
